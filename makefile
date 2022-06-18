2501b070416b675d915a94654e1d1d04fe8928bd
miq.system <miq.system@gmail.com>
2022-06-18 04:36:52 -0400

    initial commit

---
+++ makefile
@@ -0,0 +1,40 @@
+all:  clean install spring
+
+spring:
+	 mvn -o   -pl webapp/ spring-boot:run
+	 
+install: 
+	 mvn -o -T 1.5C   install
+
+notest:
+	mvn -o clean install -DskipTests=true
+	 
+clean:
+	 mvn -o clean -T 1.5C 
+
+	 
+all.net: clean install.net  spring.net
+
+
+install.net: 
+	mvn dependency:resolve-plugins dependency:go-offline
+
+	
+spring.net:
+	mvn dependency:resolve-plugins dependency:go-offline -pl webapp   spring-boot:run
+
+purge:
+	rm -rf  ~/.m2/repository/miq/
+
+purge.net:
+	mvn dependency:purge-local-repository
+
+eclipse:
+	mvn eclipse:eclipse
+
+push :	
+	git commit -a 
+	git push
+	
+pull : 
+	git pull
---
+++ webapp/src/main/java/domainapp/webapp/application/services/homepage/HomePageViewModel.java
@@ -0,0 +1,29 @@
+package domainapp.webapp.application.services.homepage;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.HomePage;
+import org.apache.isis.applib.annotation.Nature;
+
+// import domainapp.modules.simple.dom.so.SimpleObject;
+// import domainapp.modules.simple.dom.so.SimpleObjects;
+
+@DomainObject(
+        nature = Nature.VIEW_MODEL,
+        logicalTypeName = "simple.HomePageViewModel"
+        )
+@HomePage
+@DomainObjectLayout()
+public class HomePageViewModel {
+
+    public String title() {
+        return "DashBoard";
+    }
+
+
+//     @Inject SimpleObjects simpleObjects;
+}
