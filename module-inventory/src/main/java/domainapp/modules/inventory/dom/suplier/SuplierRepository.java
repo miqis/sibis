21717ef2047626a60e4c8eb4c2001a5027664391
miq.system <miq.system@gmail.com>
2022-07-08 04:55:04 -0400

    tambag suplier repository

---
+++ module-inventory/src/main/java/domainapp/modules/inventory/dom/suplier/SuplierRepository.java
@@ -0,0 +1,14 @@
+package domainapp.modules.inventory.dom.suplier;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+public interface SuplierRepository extends JpaRepository<Suplier, Long> {
+
+    List<Suplier> findByNamaContaining(final String nama);
+
+    Suplier findByNama(final String nama);
+
+}
