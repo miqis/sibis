e0ed92365f57a8fc48880fde2a915d850d58b532
miq.system <miq.system@gmail.com>
2022-07-29 05:37:49 -0400

    secman

--- webapp/src/main/java/domainapp/webapp/AppManifest.java
+++ webapp/src/main/java/domainapp/webapp/AppManifest.java
@@ -1,10 +1,5 @@
 package domainapp.webapp;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
 import org.apache.isis.core.config.presets.IsisPresets;
 import org.apache.isis.core.runtimeservices.IsisModuleCoreRuntimeServices;
 import org.apache.isis.extensions.flyway.impl.IsisModuleExtFlywayImpl;
@@ -14,6 +9,13 @@ import org.apache.isis.testing.fixtures.applib.IsisModuleTestingFixturesApplib;
 import org.apache.isis.testing.h2console.ui.IsisModuleTestingH2ConsoleUi;
 import org.apache.isis.viewer.restfulobjects.jaxrsresteasy4.IsisModuleViewerRestfulObjectsJaxrsResteasy4;
 import org.apache.isis.viewer.wicket.viewer.IsisModuleViewerWicketViewer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import org.apache.isis.extensions.secman.integration.IsisModuleExtSecmanIntegration;
+import org.apache.isis.extensions.secman.encryption.spring.IsisModuleExtSecmanEncryptionSpring;
 
 import domainapp.webapp.application.ApplicationModule;
 import domainapp.webapp.application.fixture.scenarios.DomainAppDemo;
@@ -22,26 +24,27 @@ import domainapp.webapp.quartz.QuartzModule;
 
 @Configuration
 @Import({
-        IsisModuleCoreRuntimeServices.class,
-        IsisModuleSecurityShiro.class,
-        IsisModulePersistenceJpaEclipselink.class,
-        IsisModuleViewerRestfulObjectsJaxrsResteasy4.class,
-        IsisModuleViewerWicketViewer.class,
+		// Security Manager Extension (secman)
+		IsisModuleExtSecmanIntegration.class, 
+		IsisModuleExtSecmanEncryptionSpring.class,
 
-        IsisModuleTestingFixturesApplib.class,
-        IsisModuleTestingH2ConsoleUi.class,
+		IsisModuleCoreRuntimeServices.class, 
+		IsisModuleSecurityShiro.class, 
+		IsisModulePersistenceJpaEclipselink.class,
+		IsisModuleViewerRestfulObjectsJaxrsResteasy4.class, 
+		IsisModuleViewerWicketViewer.class,
 
-        IsisModuleExtFlywayImpl.class,
+		IsisModuleTestingFixturesApplib.class, 
+		IsisModuleTestingH2ConsoleUi.class,
 
-        ApplicationModule.class,
-        CustomModule.class,
-        QuartzModule.class,
+		IsisModuleExtFlywayImpl.class,
 
-        // discoverable fixtures
-        DomainAppDemo.class
-})
-@PropertySources({
-        @PropertySource(IsisPresets.DebugDiscovery),
-})
+		ApplicationModule.class, 
+		CustomModule.class, 
+		QuartzModule.class,
+
+		// discoverable fixtures
+		DomainAppDemo.class })
+@PropertySources({ @PropertySource(IsisPresets.DebugDiscovery), })
 public class AppManifest {
 }
